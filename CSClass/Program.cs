89f704b182565dbaa7006219dd543a66e99da19e
Suyeon Lee <s2015@e-mirim.hs.kr>
2022-05-19 12:09:27 +0900

    2022-05-19 : C#

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -8,8 +8,35 @@ namespace CSClass
 {
     internal class Program
     {
+        class Car
+        {
+            int carNumber;
+            DateTime inTime;
+            DateTime ouTime;
+
+            public void SetInTime()
+            {
+                this.inTime = DateTime.Now;
+            }
+            public void SetOutTime()
+            {
+                this.ouTime = DateTime.Now;
+            }
+        }
+
+        
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble() * 10);
+
         }
     }
 }
