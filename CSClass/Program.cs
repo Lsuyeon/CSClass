aa3a57c135c71ba31bc47b9655a9d18f3c2d171a
Suyeon Lee <s2015@e-mirim.hs.kr>
2022-05-20 11:16:54 +0900

    2022-05-20

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace CSClass
 {
+    
+    /*class Program
+    {
+        class FirstClass
+        {
+
+        }
+        class SecondClass
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }*/
     internal class Program
     {
+        
         class Car
         {
             int carNumber;
@@ -24,7 +41,19 @@ namespace CSClass
             }
         }
 
-        
+        class Product
+        {
+            public static string MADEIN = "Korea";
+
+            public string name = "무명";
+            public int price = 1000;
+
+            public Product()
+            {
+                
+            }
+        }
+
         static void Main(string[] args)
         {
             Car car = new Car();
@@ -48,8 +77,14 @@ namespace CSClass
                 Console.WriteLine("Count : " + list.Count + "\ttime: " + item);    
             }
 
+            Product productA = new Product();
 
-
+            productA.name = "포켓몬빵";
+            productA.price = 1500;
+            //인스턴스 변수를 생성과 동시에 초기화 (C# 고유 문법)
+            Product productB = new Product() { name = "소금빵", price = 2000 };
+            Product productC = new Product() { price = 5000, name = "당근케이크" };
+            Product productD = new Product() { name = "보름달" };
         }
     }
 }
